bb14920a3dd4e7813eba3ae7d8f70c68e3752428
anthony <anthonydev42@gmail.com>
2023-04-19 20:26:39 +0200

    some work

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -1,12 +1,15 @@
 #ifndef FT_PRINTF_H
 #define FT_PRINTF_H
 
-# include "libft.h"
+# include "../libft/libft.h"
 # include <stdarg.h>
 # include <unistd.h>
 
 
 
 int		ft_printf(const char *format, ...);
+int	ft_printnbr(int n);
+int	ft_printpercent(void);
+int	ft_printstr(char *format);
 
 #endif
--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -9,24 +9,24 @@ int	ft_printchar(int c)
 
 int	ft_formats(va_list args, const char format)
 {
-	int	pr_length;
+	int	pr_len;
 
-	pr_length = 0;
+	pr_len = 0;
 	if (format == 'c')
-		pr_length += ft_printchar(va_arg(args, int));
+		pr_len += ft_printchar(va_arg(args, int));
 	else if (format == 's')
-		pr_length += ft_printstr(va_arg(args, char *));
+		pr_len += ft_printstr(va_arg(args, char *));
 	else if (format == 'p')
-		pr_length += ft_print_ptr(va_arg(args, unsigned long long));
+		pr_len += ft_print_ptr(va_arg(args, unsigned long long));
 	else if (format == 'd' || format == 'i')
-		pr_length += ft_printnbr(va_arg(args, int));
+		pr_len += ft_printnbr(va_arg(args, int));
 	else if (format == 'u')
-		pr_length += ft_print_unsigned(va_arg(args, unsigned int));
+		pr_len += ft_print_unsigned(va_arg(args, unsigned int));
 	else if (format == 'x' || format == 'X')
-		pr_length += ft_print_hex(va_arg(args, unsigned int), format);
+		pr_len += ft_print_hex(va_arg(args, unsigned int), format);
 	else if (format == '%')
-		pr_length += ft_printpercent();
-	return (pr_length);
+		pr_len += ft_printpercent();
+	return (pr_len);
 }
 
 int ft_printf(const char *format, ...)
